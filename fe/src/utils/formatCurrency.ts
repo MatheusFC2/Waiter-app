568f7d124e2d980f8bf158fc290bee5a2c36846a
MatheusFC2 <matheusfreitascampos@gmail.com>
2022-12-29 00:27:20 -0300

    feat: format currency utils

---
+++ fe/src/utils/formatCurrency.ts
@@ -0,0 +1,7 @@
+/* eslint-disable indent */
+export function formatCurrency (value: number) {
+    return new Intl.NumberFormat(
+        'pt-br',
+        {style: 'currency', currency: 'BRL'}
+    ).format(value);
+}
