d7d38bc367f4f11b761418037a8485893df7d0bb
MatheusFC2 <matheusfreitascampos@gmail.com>
2023-01-02 01:40:10 -0300

    feat: import fonts and main components

--- app/App.tsx
+++ app/App.tsx
@@ -1,22 +1,20 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable indent */
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { Main } from './src/Main';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const [isFontsLoaded] = useFonts({
+        'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+        'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+        'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+    });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    if (!isFontsLoaded) {
+        return null;
+    }
+
+    return (
+        <Main/>
+    );
+}
