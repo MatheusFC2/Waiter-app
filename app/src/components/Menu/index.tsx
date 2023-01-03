5ccb4e76ce21edb413f25275eb4c9d871dae430d
MatheusFC2 <matheusfreitascampos@gmail.com>
2023-01-02 22:44:53 -0300

    chore: use api for show data

--- app/src/components/Menu/index.tsx
+++ app/src/components/Menu/index.tsx
@@ -1,8 +1,40 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable indent */
 
-import { Categories } from '../Categories';
+import { FlatList } from 'react-native';
+import { products } from '../../mocks/products';
+import { Text } from '../Text';
+import { AddToCartButton, Product, ProductDetails, ProductImage, Separator } from './styles';
+import { formatCurrency } from '../../utils/formatCurrency';
+import { PlusCircle } from '../Icons/PlusCircle';
 
 export function Menu () {
-    return null;
+
+    return (
+        <FlatList
+            data={products}
+            style={{ marginTop: 32}}
+            contentContainerStyle={{ paddingHorizontal: 24}}
+            keyExtractor={product => product._id}
+            ItemSeparatorComponent={Separator}
+            renderItem={({ item: product}) => (
+                <Product>
+                    <ProductImage
+                        source={{
+                            uri: `http://192.168.0.4:3001/uploads/${product.imagePath}`,
+                        }}
+                    />
+
+                    <ProductDetails>
+                        <Text weight='600'>{product.name}</Text>
+                        <Text size={14} color='#666' style={{ marginVertical: 8 }}>{product.description}</Text>
+                        <Text size={14} weight='600'>{formatCurrency(product.price)}</Text>
+                    </ProductDetails>
+                    <AddToCartButton>
+                        <PlusCircle/>
+                    </AddToCartButton>
+                </Product>
+            )}
+        />
+    );
 }
