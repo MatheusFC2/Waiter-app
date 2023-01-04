8c107f14361dd010452d6be2c952129ff6f31346
MatheusFC2 <matheusfreitascampos@gmail.com>
2023-01-03 23:37:50 -0300

    chore: screen headerorder

--- app/src/components/Header/index.tsx
+++ app/src/components/Header/index.tsx
@@ -1,16 +1,39 @@
 /* eslint-disable linebreak-style */
-
-import { Text } from '../Text';
-import { Container } from './styles';
-
 /* eslint-disable indent */
-export function Header() {
+
+import { TouchableOpacity } from 'react-native';
+import { Text } from '../Text';
+import { Container, Content, OrderHeader, Table } from './styles';
+
+interface HeaderProps {
+    selectedTable: string;
+    onCancelOrder: () => void;
+}
+
+export function Header({ selectedTable, onCancelOrder} : HeaderProps) {
     return (
         <Container>
-            <Text>Bem vindo(a) ao </Text>
-            <Text size={24} weight="700">
-                Waiter<Text size={24}>APP</Text>
-            </Text>
+            {!selectedTable && (
+                <>
+                    <Text>Bem vindo(a) ao </Text>
+                    <Text size={24} weight="700">
+                        Waiter<Text size={24}>APP</Text>
+                    </Text>
+                </>
+            )}
+            {selectedTable && (
+                <Content>
+                    <OrderHeader>
+                        <Text size={24} weight="600">Pedido</Text>
+                        <TouchableOpacity onPress={onCancelOrder}>
+                            <Text color="#D73035" weight='600' size={14}>Cancelar Pedido</Text>
+                        </TouchableOpacity>
+                    </OrderHeader>
+                    <Table>
+                        <Text color='#666'>Mesa {selectedTable}</Text>
+                    </Table>
+                </Content>
+            )}
         </Container>
     );
 }
--- app/src/components/Header/styles.ts
+++ app/src/components/Header/styles.ts
@@ -5,3 +5,21 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
     margin: 24px 24px 0;
 `;
+
+export const Content = styled.View``;
+
+export const OrderHeader = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+export const Table = styled.View`
+    padding: 16px;
+    background: #fff;
+    border: 1px solid rgba(204, 204, 204, 0.3);
+    border-radius: 8px;
+    margin-top: 24px;
+`;
+
+
