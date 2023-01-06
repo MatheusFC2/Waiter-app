38189e55c0d14b88f8f50225a29d6096994d5b3b
MatheusFC2 <matheusfreitascampos@gmail.com>
2023-01-06 16:58:58 -0300

    fixbug: conflit name

--- app/src/components/Menu/styles.ts
+++ app/src/components/Menu/styles.ts
@@ -2,7 +2,7 @@
 import styled from 'styled-components/native';
 
 
-export const Product = styled.TouchableOpacity`
+export const ProductContainer = styled.TouchableOpacity`
     flex-direction: row;
     align-items: center;
 `;
