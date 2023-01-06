e0d943ea09dc50c76a8853a9bf8ccd46a53045b3
MatheusFC2 <matheusfreitascampos@gmail.com>
2023-01-05 21:01:35 -0300

    feat: CartItem types

---
+++ app/src/types/CartItem.ts
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+
+import { Product } from './Product';
+
+/* eslint-disable indent */
+export interface CartItem {
+    product: Product;
+    quantity: number;
+}
