f0bbffa6c6d52ee3e4caad406aee186648b41417
MatheusFC2 <matheusfreitascampos@gmail.com>
2023-01-06 21:39:31 -0300

    feat: add button Cart and create Productmodal

--- app/src/components/Menu/index.tsx
+++ app/src/components/Menu/index.tsx
@@ -4,21 +4,43 @@
 import { FlatList } from 'react-native';
 import { products } from '../../mocks/products';
 import { Text } from '../Text';
-import { AddToCartButton, Product, ProductDetails, ProductImage, Separator } from './styles';
+import { AddToCartButton, ProductContainer, ProductDetails, ProductImage, Separator } from './styles';
 import { formatCurrency } from '../../utils/formatCurrency';
 import { PlusCircle } from '../Icons/PlusCircle';
+import { ProductModal } from '../ProductModal';
+import { useState } from 'react';
+import { Product } from '../../types/Product';
+interface MenuProps {
+    onAddToCart: (product: Product) => void;
+}
 
-export function Menu () {
 
+export function Menu ({ onAddToCart }: MenuProps) {
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [selectedProduct, setSelectedProduct] = useState<null | Product>(null);
+
+    function handleOpenModal (product: Product) {
+        setIsModalVisible(true);
+        setSelectedProduct(product);
+    }
     return (
-        <FlatList
+        <>
+            <ProductModal
+                visible={isModalVisible}
+                onClose={() => setIsModalVisible(false)}
+                product={selectedProduct}
+                onAddToCart={onAddToCart}
+            />
+
+            <FlatList
             data={products}
             style={{ marginTop: 32}}
             contentContainerStyle={{ paddingHorizontal: 24}}
             keyExtractor={product => product._id}
             ItemSeparatorComponent={Separator}
             renderItem={({ item: product}) => (
-                <Product>
+                <ProductContainer onPress={() => handleOpenModal(product)}>
                     <ProductImage
                         source={{
                             uri: `http://192.168.0.4:3001/uploads/${product.imagePath}`,
@@ -30,11 +52,13 @@ export function Menu () {
                         <Text size={14} color='#666' style={{ marginVertical: 8 }}>{product.description}</Text>
                         <Text size={14} weight='600'>{formatCurrency(product.price)}</Text>
                     </ProductDetails>
-                    <AddToCartButton>
+                    <AddToCartButton onPress={() => onAddToCart(product)}>
                         <PlusCircle/>
                     </AddToCartButton>
-                </Product>
+                </ProductContainer>
             )}
-        />
+            />
+        </>
+
     );
 }
