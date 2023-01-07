5d4328ddf4b9455a18ea105836570fe957073863
MatheusFC2 <matheusfreitascampos@gmail.com>
2023-01-07 17:08:33 -0300

    feat: finished Order

---
+++ app/src/components/OrderConfirmedModal/index.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+
+import { Modal } from 'react-native';
+import { CheckCircle } from '../Icons/CheckCircle';
+import { Container, OkButton } from './styles';
+import { Text } from '../Text';
+import { StatusBar } from 'expo-status-bar';
+
+interface OrderConfirmModalProps {
+    visible: boolean;
+    onOk: () => void;
+}
+
+export function OrderConfirmModal ({ visible, onOk } : OrderConfirmModalProps) {
+    return (
+        <Modal
+            visible={visible}
+            animationType="fade"
+        >
+
+            <StatusBar style='light'/>
+            <Container>
+                <CheckCircle/>
+                <Text
+                    size={20}
+                    weight="600"
+                    color='#fff'
+                    style={{ marginTop: 12}}
+                >Pedido Confirmado</Text>
+                <Text
+                    weight="600"
+                    color='#fff'
+                    opacity={0.9}
+                    style={{ marginTop: 12}}
+                >O pedido já entrou na fila de produção!</Text>
+
+
+                <OkButton onPress={onOk}>
+                    <Text color='#D73035' weight='600'>OK</Text>
+                </OkButton>
+            </Container>
+        </Modal>
+    );
+}
---
+++ app/src/components/OrderConfirmedModal/styles.ts
@@ -0,0 +1,17 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    background: #D73035;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const OkButton = styled.TouchableOpacity`
+    margin-top: 24px;
+    padding: 14px 24px;
+    background: #fff;
+    border-radius: 48px;
+`;
