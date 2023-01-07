f1f6e19874627a9d7c4bd464a888097684c25487
MatheusFC2 <matheusfreitascampos@gmail.com>
2023-01-06 22:28:48 -0300

    feat: create function to buttons and imgs

--- app/src/Main/index.tsx
+++ app/src/Main/index.tsx
@@ -13,20 +13,80 @@ import { Header } from '../components/Header';
 import { Menu } from '../components/Menu';
 import { Button } from '../components/Button';
 import { TableModal } from '../components/TableModal';
+import { Cart } from '../components/Cart';
+import { CartItem } from '../types/CartItem';
+import { Product } from '../types/Product';
 
 
 export function Main() {
 
     const [isTableModalVisible, setIsTableModalVisible] = useState(false);
     const [selectedTable, setSelectedTable] = useState('');
+    const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
     function handleSaveTable (table: string) {
         setSelectedTable(table);
+        setIsTableModalVisible(false);
     }
 
     function handleCancelOrder () {
         setSelectedTable('');
+        setCartItems([]);
     }
+
+    function handleAddToCart (product: Product) {
+        if (!selectedTable) {
+            setIsTableModalVisible(true);
+        }
+
+        setCartItems((prevState) => {
+            const itemIndex = prevState.findIndex(
+                cartItem => cartItem.product._id === product._id
+            );
+
+            if (itemIndex < 0) {
+                return prevState.concat({
+                    quantity: 1,
+                    product,
+                });
+            }
+
+            const newCartItems = [...prevState];
+            const item = newCartItems[itemIndex];
+
+            newCartItems[itemIndex] = {
+                ...item,
+                quantity: item.quantity + 1,
+            };
+
+            return newCartItems;
+        });
+    }
+
+    function handleDrecrementCartItems(product: Product) {
+        setCartItems((prevState) => {
+            const itemIndex = prevState.findIndex(
+                cartItem => cartItem.product._id === product._id
+            );
+
+            const item = prevState[itemIndex];
+            const newCartItems = [...prevState];
+
+            if (item.quantity === 1) {
+                newCartItems.splice(itemIndex, 1);
+
+                return newCartItems;
+            }
+
+            newCartItems[itemIndex] = {
+                ...item,
+                quantity: item.quantity - 1,
+            };
+
+            return newCartItems;
+        });
+    }
+
     return (
         <>
             <Container>
@@ -40,7 +100,7 @@ export function Main() {
                 </CategoriesContainer>
 
                 <MenuContainer>
-                    <Menu/>
+                    <Menu onAddToCart={handleAddToCart}/>
                 </MenuContainer>
 
             </Container>
@@ -50,6 +110,14 @@ export function Main() {
                         <Button onPress={() => setIsTableModalVisible(true)} disabled={false}>
                             Novo Pedido
                         </Button>
+                    )}
+
+                    {selectedTable && (
+                        <Cart
+                            cartItems={cartItems}
+                            onAdd={handleAddToCart}
+                            onDecrement={handleDrecrementCartItems}
+                        />
                     )}
                 </FooterContainer>
             </Footer>
