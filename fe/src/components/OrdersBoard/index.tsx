5c2e36469c1e9e3835b4f5fc2cc02646c321e078
MatheusFC2 <matheusfreitascampos@gmail.com>
2023-01-12 00:12:58 -0300

    feat: cancel order

--- fe/src/components/OrdersBoard/index.tsx
+++ fe/src/components/OrdersBoard/index.tsx
@@ -5,16 +5,19 @@ import { useState } from 'react';
 import { Order } from '../../types/Order';
 import { OrderModal } from '../OrderModal';
 import { Board, OrdersContainer } from './styles';
+import { api } from '../../utils/api';
 
 interface OrderBoardProps {
     icon: string;
     title: string;
     orders: Order[];
+    onCancelOrder: (orderId: string) => void;
 }
 
-export function OrdersBoard({icon, title, orders}: OrderBoardProps) {
+export function OrdersBoard({icon, title, orders, onCancelOrder}: OrderBoardProps) {
     const [isModalVisible, setIsModalVisible] = useState(false);
     const [selectedOrder, setSelectedOrder] = useState<null | Order>(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     function handleOpenModal(order: Order) {
         setIsModalVisible(true);
@@ -26,17 +29,32 @@ export function OrdersBoard({icon, title, orders}: OrderBoardProps) {
         setSelectedOrder(null);
     }
 
+    async function handleCancelOrder() {
+        setIsLoading(true);
+
+        await new Promise(resolve => setTimeout(resolve, 3000)),
+
+        await api.delete(`/orders/${selectedOrder?._id}`);
+
+        onCancelOrder(selectedOrder!._id);
+        setIsLoading(false);
+        setIsModalVisible(false);
+    }
+
     return (
     <Board>
         <OrderModal
             visible={isModalVisible}
             order={selectedOrder}
             onClose={handleCloseModal}
+            onCancelOrder={handleCancelOrder}
+            isLoading={isLoading}
+
         />
       <header>
         <span>{icon}</span>
         <strong>{title}</strong>
-        <span>(1)</span>
+        <span>({orders.length})</span>
       </header>
 
       {orders.length > 0 && (
@@ -44,7 +62,7 @@ export function OrdersBoard({icon, title, orders}: OrderBoardProps) {
           {orders.map((order) => (
             <button type='button' key={order._id} onClick={() => handleOpenModal(order)}>
               <strong>Mesa {order.table}</strong>
-              <span>{order.products.length}</span>
+              <span>{order.products.length} itens</span>
             </button>
           ))}
         </OrdersContainer>
